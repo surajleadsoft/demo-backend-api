028c36573dfe67e82d232a286b253055820cc291
Suraj Pawar <84491498+surajleadsoft@users.noreply.github.com>
2025-12-31 12:04:06 +0530

    Update connection.js

--- utils/connection.js
+++ utils/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect('mongodb+srv://surajleadsoft_db_user:LeadSoft%40123@products.8jetvbj.mongodb.net/product_db')
+        await mongoose.connect('mongodb+srv://surajleadsoft_db_user:LeadSoft%40123@products.8jetvbj.mongodb.net/')
         console.log('Database connected !!')
     }catch(error){
         console.log(error)
